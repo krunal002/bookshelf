0280b178656b0da7484a386af764d4b9bfedde64
krunal002 <109591661+krunal002@users.noreply.github.com>
2024-03-14 19:59:59 +0530

    books can be categorized

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
@@ -12,14 +13,14 @@ function App() {
     {
       name: "book 2",
       author: "writer1",
-      category: "a",
+      category: "b",
       cover:
         "https://th.bing.com/th/id/OIP.0JmYG5E90CH44HqiMM05iwHaLH?rs=1&pid=ImgDetMain",
     },
     {
       name: "book 3",
       author: "writer1",
-      category: "a",
+      category: "c",
       cover:
         "https://th.bing.com/th/id/OIP.0JmYG5E90CH44HqiMM05iwHaLH?rs=1&pid=ImgDetMain",
     },
@@ -32,15 +33,36 @@ function App() {
     },
   ];
 
+  const [reqCat, setReqCat] = useState("all");
+
+  const dataToShow =
+    reqCat === "all" ? books : books.filter((book) => book.category === reqCat);
+
   return (
     <div className="App">
       <h1>Bokshelf</h1>
-      
-      {books.map((book) => (
-        <section>
-          <p>{book.name}</p>
-        </section>
-      ))}
+
+      <nav>
+        <button onClick={(e) => setReqCat("all")}>cat all</button>
+        <button onClick={(e) => setReqCat("a")}>cat a</button>
+        <button onClick={(e) => setReqCat("b")}>cat b</button>
+        <button onClick={(e) => setReqCat("c")}>cat c</button>
+      </nav>
+
+      <main className="primary-container">
+        {dataToShow.map((book) => (
+          <section className="book-wrapper">
+            <img
+              src={book.cover}
+              alt="book-cover"
+              className="book-cover-wrapper"
+            />
+            <p>{book.name}</p>
+            <p>{book.author}</p>
+            <p>{book.category}</p>
+          </section>
+        ))}
+      </main>
     </div>
   );
 }
